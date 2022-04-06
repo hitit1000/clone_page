75cb83d30487e0d7141de67e79013979ab234a24
hitit1000 <hitit1000@naver.com>
2022-04-06 21:52:45 +0900

    study resposive nav finish

--- 01_sugar_studios/index.js
+++ 01_sugar_studios/index.js
@@ -6,16 +6,16 @@ let scaleValue;
 
 const changeScale = () => {
   if (window.innerWidth >= 1216) {
-    console.log("컴퓨터");
     body.style.width = `${window.innerWidth}px`;
     body.style.transform = `scale(1)`;
   } else if (785 <= window.innerWidth && window.innerWidth <= 1215) {
-    console.log("태블릿");
     scaleValue = 1 - (1215 - window.innerWidth) / 1215;
     body.style.transform = `scale(${scaleValue})`;
     body.style.width = `${window.innerWidth / scaleValue}px`;
   } else if (window.innerWidth <= 784) {
-    console.log("휴대폰");
+    scaleValue = 1 - (785 - window.innerWidth) / 785;
+    body.style.transform = `scale(${scaleValue})`;
+    body.style.width = `${window.innerWidth / scaleValue}px`;
   } else {
     console.log("javascript resize error");
   }
