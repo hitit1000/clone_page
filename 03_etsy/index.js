af83c4f316fc3951ba46419b627bf15bea86cc11
hitit1000 <hitit1000@naver.com>
2022-04-22 14:16:53 +0900

    edit effect section1

--- 03_etsy/index.js
+++ 03_etsy/index.js
@@ -1,22 +1,26 @@
-const body = document.querySelector("body");
-const imgs = document.querySelectorAll(".scaleImage");
-let scaleValue;
-const imgs_array = Object.entries(imgs).map((img) => img[1]);
-console.log(imgs_array);
+// const body = document.querySelector("body");
+// const imgs = document.querySelectorAll(".scaleImage");
+// let scaleValue;
+// const imgs_array = Object.entries(imgs).map((img) => img[1]);
+// console.log(imgs_array);
 
-const changeScale = () => {
-  if (window.innerWidth >= 368) {
-    imgs_array.map((img) => {
-      img.style.transform = `scale(1)`;
-    });
-  } else if (window.innerWidth < 368) {
-    scaleValue = 1 - (368 - window.innerWidth) / 368;
-    imgs_array.map((img) => {
-      img.style.transform = `scale(${scaleValue})`;
-    });
-  } else {
-    console.log("javascript resize error");
-  }
-};
+// const changeScale = () => {
+//   if (window.innerWidth >= 368) {
+//     imgs_array.map((img) => {
+//       // img.style.transform = `scale(1)`;
+//     });
+//   } else if (window.innerWidth < 368) {
+//     scaleValue = 1 - (368 - window.innerWidth) / 368;
+//     imgs_array.map((img) => {
+//       img.style.transform = `scale(${scaleValue})`;
+//     });
+//   } else {
+//     console.log("javascript resize error");
+//   }
+// };
 
-window.addEventListener("resize", changeScale);
+// const hoverEvent = () => {
+//   console.timeLog("hgu");
+// };
+
+// window.addEventListener("resize", changeScale);
